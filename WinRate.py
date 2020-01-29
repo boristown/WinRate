43e847e98768f8528c27c3c8f051a6f6095cd84e
Boris Town <boristown@outlook.com>
2020-01-29 23:32:53 +0800

    迭代6：添加CSV Header

--- WinRate.py
+++ WinRate.py
@@ -1,9 +1,17 @@
 import glob
 import json
+import csv
 
+#Open config.json
 f = open("config.json", encoding='utf-8')
+#Read config.json
 config = json.load(f)
 print(config)
+#Load Prices Files 
 pricesPath = glob.glob(config["PricesFiles"])
 print(pricesPath)
-validationFile = open(config["ValidationFile"],'w')
+#Create ValidationFile.csv File
+with open(config["ValidationFile"],"w") as validationFile:
+    writer = csv.writer(validationFile)
+    writer.writerow(["Steps","Total","Win","Loss","WinRate"])
+    
