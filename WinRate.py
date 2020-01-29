d5629a8ef3226c0a67fea51167b32e7060008a61
Boris Town <boristown@outlook.com>
2020-01-29 23:05:18 +0800

    迭代5：创建Validation文件

--- WinRate.py
+++ WinRate.py
@@ -4,3 +4,6 @@ import json
 f = open("config.json", encoding='utf-8')
 config = json.load(f)
 print(config)
+pricesPath = glob.glob(config["PricesFiles"])
+print(pricesPath)
+validationFile = open(config["ValidationFile"],'w')
