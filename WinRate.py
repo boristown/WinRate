37971a130d23b5cd42393a1ee67e05e6f705e39c
Boris Town <boristown@outlook.com>
2020-01-30 14:14:07 +0800

    迭代7：读取价格数据

--- WinRate.py
+++ WinRate.py
@@ -7,9 +7,17 @@ f = open("config.json", encoding='utf-8')
 #Read config.json
 config = json.load(f)
 print(config)
-#Load Prices Files 
+#Glob Prices Files 
 pricesPath = glob.glob(config["PricesFiles"])
 print(pricesPath)
+symbol_list = []
+#Read Prices Files
+for fileIndex, pricePath in enumerate(pricesPath):
+    with open(pricePath) as priceFile:
+        price_lines = priceFile.read().splitlines()
+        price_list = list(map(float, price_lines))
+        symbol_list.append(price_list)
+        print(len(symbol_list))
 #Create ValidationFile.csv File
 with open(config["ValidationFile"],"w") as validationFile:
     writer = csv.writer(validationFile)
