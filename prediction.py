1e000e26e610b678cac444c83338f5dd1d15d337
Boris Town <boristown@outlook.com>
2020-01-31 22:41:06 +0800

    迭代22：多阶胜率

--- prediction.py
+++ prediction.py
@@ -1,15 +1,14 @@
 import glob
 import time
 import csv
+import math
 
 def create_resultlist(config):
     resultlist = []
-    resultlist.append(0) #0 Steps
-    resultlist.append(0) #1 Total
-    resultlist.append(0) #2 Win
-    resultlist.append(0) #3 Loss
-    resultlist.append(0.0) #4 WinRate
-    resultlist.append(0.0) #5 Score
+    for i in range(21):
+        resultlist.append(0) #Steps,Total,Win
+    for i in range(10):
+        resultlist.append(0.0) #WinRate
     return resultlist
 
 def calculate_winrate_fromfile(config, validationList, steps, resultlist, prediction_file):
@@ -21,16 +20,17 @@ def calculate_winrate_fromfile(config, validationList, steps, resultlist, predic
         resultlist[0] += 1 #0 Steps
         resultlist[1] += config["batch"] #1 Total
         for row in csvreader:
-            riseflag = 1 if (float)(row[1]) >= 0.5 else 0
+            predictValue = (float)(row[1])
+            predictScoreIndex = math.floor(abs((float)(row[1]) * 2 - 1) * 10.0)
+            resultlist[predictScoreIndex + 1] += 1
+            riseflag = 1 if predictValue >= 0.5 else 0
             if riseflag == validationList[predict_index]:
-                resultlist[2] += 1 #2 Win
-            else:
-                resultlist[3] += 1 #3 Loss
+                resultlist[predictScoreIndex + 11] += 1 #2 Win
             predict_index += 1
             if predict_index >= prices_batch_size:
                 break
-            resultlist[5] = abs((float)(row[1]) * 2 - 1) * 100.0
-        resultlist[4] = resultlist[2] / resultlist[1] #4 WinRate
+        for i in range(10):
+            resultlist[i + 21] = resultlist[i + 11] / resultlist[i + 1] if resultlist[i + 1] > 0 else 0.5 #4 WinRate
     return resultlist
 
 def calculate_winrate(config, validationList, resultlist, steps):
--- results.py
+++ results.py
@@ -4,7 +4,10 @@ def create(config):
     with open(config["WinRateFile"],"w",newline='') as winrateFile:
         winrateWriter = csv.writer(winrateFile)
         #Write Header
-        winrateWriter.writerow(["Steps","Total","Win","Loss","WinRate","Score"])
+        winrateWriter.writerow(["Steps",
+                                "Total","Total10","Total20","Total30","Total40","Total50","Total60","Total70","Total80","Total90",
+                                "Win","Win10","Win20","Win30","Win40","Win50","Win60","Win70","Win80","Win90",
+                                "WinRate","WinRate10","WinRate20","WinRate30","WinRate40","WinRate50","WinRate60","WinRate70","WinRate80","WinRate90"])
 
 def append(config, resultline):
     with open(config["WinRateFile"],"a",newline='') as winrateFile:
