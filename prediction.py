210b11694a108cb0967fad2d8c132046113480b7
Boris Town <boristown@outlook.com>
2020-02-02 13:21:26 +0800

    迭代23：胜率计算修复

--- prediction.py
+++ prediction.py
@@ -5,9 +5,9 @@ import math
 
 def create_resultlist(config):
     resultlist = []
-    for i in range(21):
+    for i in range(23):
         resultlist.append(0) #Steps,Total,Win
-    for i in range(10):
+    for i in range(11):
         resultlist.append(0.0) #WinRate
     return resultlist
 
@@ -18,19 +18,21 @@ def calculate_winrate_fromfile(config, validationList, steps, resultlist, predic
         predict_index = 0
         time.sleep(5)
         resultlist[0] += 1 #0 Steps
-        resultlist[1] += config["batch"] #1 Total
         for row in csvreader:
             predictValue = (float)(row[1])
-            predictScoreIndex = math.floor(abs((float)(row[1]) * 2 - 1) * 10.0)
-            resultlist[predictScoreIndex + 1] += 1
+            predictScore = (float)(row[1]) * 2 - 1
+            predictScoreIndex = math.floor(abs(predictScore * 10.0))
+            resultlist[1] += 1 #Total
+            resultlist[predictScoreIndex + 2] += 1 #Total
             riseflag = 1 if predictValue >= 0.5 else 0
             if riseflag == validationList[predict_index]:
-                resultlist[predictScoreIndex + 11] += 1 #2 Win
+                resultlist[12] += 1 #2 Win
+                resultlist[predictScoreIndex + 13] += 1 #2 Win
             predict_index += 1
             if predict_index >= prices_batch_size:
                 break
-        for i in range(10):
-            resultlist[i + 21] = resultlist[i + 11] / resultlist[i + 1] if resultlist[i + 1] > 0 else 0.5 #4 WinRate
+        for i in range(11):
+            resultlist[i + 23] = resultlist[i + 12] / resultlist[i + 1] if resultlist[i + 1] > 0 else 0.5 #4 WinRate
     return resultlist
 
 def calculate_winrate(config, validationList, resultlist, steps):
--- results.py
+++ results.py
@@ -5,9 +5,9 @@ def create(config):
         winrateWriter = csv.writer(winrateFile)
         #Write Header
         winrateWriter.writerow(["Steps",
-                                "Total","Total10","Total20","Total30","Total40","Total50","Total60","Total70","Total80","Total90",
-                                "Win","Win10","Win20","Win30","Win40","Win50","Win60","Win70","Win80","Win90",
-                                "WinRate","WinRate10","WinRate20","WinRate30","WinRate40","WinRate50","WinRate60","WinRate70","WinRate80","WinRate90"])
+                                "Total","Total0","Total10","Total20","Total30","Total40","Total50","Total60","Total70","Total80","Total90",
+                                "Win","Win0","Win10","Win20","Win30","Win40","Win50","Win60","Win70","Win80","Win90",
+                                "WinRate","WinRate0","WinRate10","WinRate20","WinRate30","WinRate40","WinRate50","WinRate60","WinRate70","WinRate80","WinRate90"])
 
 def append(config, resultline):
     with open(config["WinRateFile"],"a",newline='') as winrateFile:
